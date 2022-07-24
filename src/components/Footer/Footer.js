d46828136a02a7b4006fb9591888092ee3de1c80
Batya <Gigi.ucla21@gmail.com>
2022-07-24 16:52:46 -0700

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(props) {
     return (
         <footer className='footer'>
-            <p>By Gigi. Copyright {props.year}</p>
+            <p>Created by Gigi. Copyright {props.year}</p>
         </footer>
     );
 }
