164991ba5db85262e3701b602f528d0a91f6b246
Batya <Gigi.ucla21@gmail.com>
2022-07-25 11:49:17 -0700

    update css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => (
     <div>
         <header className="App-header">
-            <h1>News Feed</h1>
+            <h1 className='App-title' >News Feed</h1>
             <p className='header-tag-line'>Know the world around you!</p>
         </header>
     </div>
